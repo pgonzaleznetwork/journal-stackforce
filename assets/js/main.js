1af3d8f0443b9a2f4f9aa39bbba61cf2ef99c554
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2022-07-19 00:50:54 +0100

    added code to allow burger to show menu in mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
-(function () {
-    pagination(false);
-})();
+let burger = document.querySelector('.gh-burger');
+burger.addEventListener('click',() => {
+
+    if(document.body.classList.contains("is-head-open")){
+        console.log('removing class')
+        document.body.classList.remove("is-head-open")
+    }
+    else{
+        console.log('adding class')
+        document.body.classList.add("is-head-open");
+    }     
+})
